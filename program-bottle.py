2ce2d177e87252a8e5318ca0ff2d0950a5800290
javii46 <javi_14_dh@hotmail.com>
2014-06-04 19:12:06 +0200

    inicio programa bottle

---
+++ program-bottle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import json
+import requests
+from bottle import route, get, post, run, template, debug, error, request, static_file, default_app, TEMPLATE_PATH
+import os
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./static')
+
+@error(404)
+def error404(error):
+    return template('error')
+
+@route('/')
+def search():
+    return template('inicio')
+
+@post('/resultado')
+def nombre():
+    global artist
+    artist = request.forms.get("artista")
+    r = requests.get('http://api.deezer.com/search/album', params={'q':'%s'% artist})
+    resultado=json.loads(r.text)
+    return template('results', data=resultado)
+
+debug='TRUE'
+run(host='localhost', port=8080)
+
+
