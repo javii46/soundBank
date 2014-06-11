eb6414aa70472663c6d916be562a7e374197924a
javii46 <javi_14_dh@hotmail.com>
2014-06-11 14:24:57 +0200

    añadimos requests aotra api

--- program-bottle.py
+++ program-bottle.py
@@ -5,6 +5,7 @@ import json
 import requests
 from bottle import route, get, post, run, template, debug, error, request, static_file, default_app, TEMPLATE_PATH
 import os
+from lxml import etree
 
 @route('/static/<filename>')
 def server_static(filename):
@@ -25,6 +26,18 @@ def nombre():
     r = requests.get('http://api.deezer.com/search/album', params={'q':'%s'% artist})
     datos=json.loads(r.text)
     return template('results', data=datos,busqueda=artist)
+
+@post('/letra')
+def letra():
+    artist2 = request.forms.get("artista2")
+    cancion = request.forms.get("cancion")
+    dicc_peticion = {'artist':artist2,'song':cancion,'fmt':'xml'}
+    peticion = requests.get('http://lyrics.wikia.com/api.php?', params=dicc_peticion)
+    datos1 = etree.fromstring(peticion.text.encode("utf-8"))
+    url = datos1.find("url")
+    lyrics = datos1.find("lyrics")
+    song = datos1.find("song")	
+    return template('letra', url=url.text,lyrics=lyrics.text,song=song.text)
 
 debug='TRUE'
 run(host='localhost', port=8080)
